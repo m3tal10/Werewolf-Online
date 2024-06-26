cb37e330d269739dc554f9225a1087661fe3f41b
Ali Ashraf Abir <fazilabir88@gmail.com>
2024-06-26 06:10:17 -0700

    update 3

--- src/components/Pages/Lobby.jsx
+++ src/components/Pages/Lobby.jsx
@@ -19,12 +19,31 @@ export default function Lobby() {
     fetch(`http://localhost:5000/lobby/${roomName?roomName:userName}`)
     .then(res=>res.json())
     .then(data=>setPlayerData(data))
-    console.log(playerData)
     setAction(null)
   }, [temp])
 
 
-  
+  const handleChat=()=>{
+
+    const message=document.getElementById('chat').value
+    const user=userName
+
+    console.log(message)
+
+    fetch(`http://localhost:5000/chat/${roomName?roomName:userName}`,{
+      method:'POST',
+      headers:{'content-type':'application/json'},
+      body: JSON.stringify({message,user}),
+
+  })
+  .then(res=>res.json())
+  .then(data=>{
+
+
+  })
+
+
+  }
 
   
 
@@ -41,12 +60,12 @@ export default function Lobby() {
 
     </div>
 
-    <div className="h-[32vh] w-[100wh] border-2 border-red-500">
-
+    <div className="h-[32vh] w-[100wh] border-2 border-red-500 overflow-y-scroll">
+    {playerData[0]?.Chats?.map(chat=><p className='text-[3vh]'><span className='text-bold text-red-400 '>{chat.body.user}:</span> {chat.body.message}</p>)}
     </div>
     <div className="flex">
-    <input className='w-[100vw] h-[8vh]' type="text" />
-    <button>send</button>
+    <input id='chat' className='w-[100vw] h-[8vh]' type="text" />
+    <button onClick={handleChat}>send</button>
     </div>
 
 </>
